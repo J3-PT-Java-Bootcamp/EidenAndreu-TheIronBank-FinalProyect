45c856392a5aa09e3ed632a5ae581c86c0418387
EidenAndreu <eidenreinaldo@gmail.com>
2022-09-03 16:24:47 +0200

    First commit

---
+++ src/main/java/com/example/theironbankfinalproyect/TheIronBankFinalProyectApplication.java
@@ -0,0 +1,13 @@
+package com.example.theironbankfinalproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheIronBankFinalProyectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheIronBankFinalProyectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/theironbankfinalproyect/TheIronBankFinalProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.theironbankfinalproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheIronBankFinalProyectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
