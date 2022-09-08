cf57025c8b5d72d8e2cbd2e07bd6ebe8ef78452c
EidenAndreu <eidenreinaldo@gmail.com>
2022-09-08 18:41:23 +0200

    Dto created

---
+++ src/main/java/com/example/theironbankfinalproyect/dto/users/AdminDto.java
@@ -0,0 +1,14 @@
+package com.example.theironbankfinalproyect.dto.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AdminDto {
+    String name;
+}
