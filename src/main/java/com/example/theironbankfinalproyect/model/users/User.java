758ef27dfc8be5f3a617d91b1ecbc8f604952f71
EidenAndreu <eidenreinaldo@gmail.com>
2022-09-10 11:07:42 +0200

    Update Model User

--- src/main/java/com/example/theironbankfinalproyect/model/users/User.java
+++ src/main/java/com/example/theironbankfinalproyect/model/users/User.java
@@ -23,50 +23,11 @@ public class User {
     @Column(name = "id", nullable = false)
     private long id;
 
-    @OneToMany(mappedBy = "user" )
-    private List<Checking> checking;
+
+
     @Column (name = "name")
     private String name;
 
-    @Column(name = "date_of_birth")
-    private Date dateOfBirth;
 
-    @Embedded
-    private Address address;
 
-    @Column(name = "phone_number")
-    private String phoneNumber;
-
-    @Column(name = "email")
-    private String email;
-    private String hashedKey;
-
-    public User(String name, Date dateOfBirth, Address address, String phoneNumber, String email, String hashedKey) {
-        this.name = name;
-        this.dateOfBirth = dateOfBirth;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.hashedKey = hashedKey;
-    }
-
-    public User(Long id, String name, Date dateOfBirth, Address address, String phoneNumber, String email) {
-        this.id = id;
-        this.name = name;
-        this.dateOfBirth = dateOfBirth;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
-
-    public User(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public User(Long id, String name, String hashedKey) {
-        this.id = id;
-        this.name = name;
-        this.hashedKey = hashedKey;
-    }
 }
