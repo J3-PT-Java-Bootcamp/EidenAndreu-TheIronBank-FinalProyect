aa2928b422399f48f65a76a70b84c493af7418d4
EidenAndreu <eidenreinaldo@gmail.com>
2022-09-10 17:52:40 +0200

    Controllers created

--- src/main/java/com/example/theironbankfinalproyect/controller/users/UserController.java
+++ src/main/java/com/example/theironbankfinalproyect/controller/users/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/accountHolder")
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
